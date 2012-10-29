1ca48dfedd81e8ed92e894e5c110d0c9c707ab48
Qiankun Miao <qiankun.miao@intel.com>
2012-10-29 12:48:47 +0800

    Draw a triangle to screen

--- rec.cpp
+++ rec.cpp
@@ -1,10 +1,22 @@
 #include <GL/glut.h>
 
-void myDisplay(void)
+
+void myDisplay1(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glRectf(-0.5f, -0.5f, 0.5f, 0.5f);
     glFlush();
+}
+
+void myDisplay()
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+  glBegin(GL_TRIANGLES);
+    glVertex3f(-1.0f,-1.0f,0.0f);
+    glVertex3f(1.0f,-1.0f,0.0f);
+    glVertex3f(0.0f,0.0f,0.0f);
+  glEnd();
+  glFlush();
 }
 
 int main(int argc, char *argv[])
